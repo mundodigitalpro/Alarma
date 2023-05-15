e649e6e7a5d474cefee9e39778e1abd47e4e4300
Jose Jordan <mundodigital.pro@gmail.com>
2023-05-15 12:49:03 +0200

    Wakelock and Notification are implemented

--- app/src/main/java/com/josejordan/alarma/MotionDetectionService.kt
+++ app/src/main/java/com/josejordan/alarma/MotionDetectionService.kt
@@ -1,11 +1,20 @@
 package com.josejordan.alarma
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
+import android.os.Build
 import android.os.IBinder
+import android.os.PowerManager
+import android.util.Log
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
@@ -23,19 +32,80 @@ class MotionDetectionService : Service(), LifecycleOwner {
     private var isServiceStarted = false
     private var cameraProvider: ProcessCameraProvider? = null
     private var imageAnalysis: ImageAnalysis? = null
+    private var wakeLock: PowerManager.WakeLock? = null  // Variable miembro para el WakeLock
+
+    companion object {
+        const val CHANNEL_ID = "MotionDetectionChannel"
+        const val NOTIFICATION_ID = 1
+        const val CHANNEL_DESCRIPTION = "Canal para la notificación de detección de movimiento"
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = CHANNEL_ID
+            val descriptionText = CHANNEL_DESCRIPTION
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
 
     override fun onBind(intent: Intent): IBinder? {
         return null
     }
 
+
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         if (!isServiceStarted) {
             lifecycleRegistry.currentState = Lifecycle.State.STARTED
+
+            // Intenta adquirir el WakeLock
+            try {
+                val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+                wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyApp::MyWakelockTag")
+                wakeLock?.acquire()
+            } catch (e: SecurityException) {
+                Log.e("MotionDetectionService", "Error adquiriendo WakeLock", e)
+                // Aquí puedes manejar el error, por ejemplo, puedes detener el servicio si el WakeLock es esencial para tu aplicación
+                //stopSelf()
+                //return START_NOT_STICKY
+            }
+
+            // Crea una notificación y establece el servicio en primer plano
+            val notificationIntent = Intent(this, MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(
+                this,
+                0, notificationIntent, PendingIntent.FLAG_IMMUTABLE
+            )
+
+            val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Detección de Movimiento Activada")
+                .setContentText("La detección de movimiento está funcionando en segundo plano.")
+                .setSmallIcon(R.drawable.ic_launcher_foreground) // Cambia esto por el ícono de notificación que desees
+                .setContentIntent(pendingIntent)
+                .build()
+
+
+            startForeground(NOTIFICATION_ID, notification)
+
             startCamera()
             isServiceStarted = true
         }
         return START_STICKY
     }
+
 
     override fun onDestroy() {
         super.onDestroy()
@@ -46,6 +116,12 @@ class MotionDetectionService : Service(), LifecycleOwner {
 
         imageAnalysis?.clearAnalyzer()
         cameraProvider?.unbindAll()
+
+        // Verifica si el WakeLock está encendido antes de liberarlo
+        if (wakeLock?.isHeld == true) {
+            wakeLock?.release()
+        }
+
     }
 
     override fun getLifecycle(): Lifecycle {
@@ -55,7 +131,7 @@ class MotionDetectionService : Service(), LifecycleOwner {
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
-        cameraProviderFuture.addListener(Runnable {
+        cameraProviderFuture.addListener({
             cameraProvider = cameraProviderFuture.get()
             bindImageAnalysis(cameraProvider!!)
         }, ContextCompat.getMainExecutor(this))
@@ -81,12 +157,14 @@ class MotionDetectionService : Service(), LifecycleOwner {
         if (mediaPlayer == null) {
             mediaPlayer = MediaPlayer.create(this, R.raw.alarm_sound)
         }
-        if (!mediaPlayer?.isPlaying!!) {
-            mediaPlayer?.start()
+        mediaPlayer?.let { player ->
+            if (!player.isPlaying) {
+                player.start()
+            }
         }
     }
 
-    //octavo mejorado
+    //octavo MotionDetectionAnalyzer mejorado
     inner class MotionDetectionAnalyzer(private val onMotionDetected: () -> Unit) : ImageAnalysis.Analyzer {
         private val THRESHOLD = 5000  // Increased threshold
         private val lastFrames = LinkedList<ByteBuffer>()
@@ -107,10 +185,10 @@ class MotionDetectionService : Service(), LifecycleOwner {
                 lastFrames.remove()
             }
 
-            try {
+            image.use {
                 if (lastFrames.size == FRAME_BUFFER_SIZE) {
                     val firstFrame = lastFrames.peek()
-                    if (hasMotion(firstFrame, currentFrame)) {
+                    if (hasMotion(firstFrame as ByteBuffer, currentFrame)) {
                         val currentTriggerTime = System.currentTimeMillis()
                         if (currentTriggerTime - lastTriggerTime >= MIN_TIME_BETWEEN_TRIGGERS) {
                             onMotionDetected()
@@ -118,8 +196,6 @@ class MotionDetectionService : Service(), LifecycleOwner {
                         }
                     }
                 }
-            } finally {
-                image.close()
             }
         }
 
@@ -135,287 +211,5 @@ class MotionDetectionService : Service(), LifecycleOwner {
             val averageDiff = diff / (previousFrame.limit().toDouble())
             return averageDiff > THRESHOLD
         }
-    }
-
-
-    //septimo
-/*    inner class MotionDetectionAnalyzer(private val onMotionDetected: () -> Unit) : ImageAnalysis.Analyzer {
-        private val THRESHOLD = 5000  // Increased threshold
-        private var lastFrames: Queue<ByteBuffer> = LinkedList()
-        private var lastTriggerTime = 0L
-        private val MIN_TIME_BETWEEN_TRIGGERS = 10000 // Increased minimum time between triggers
-        private val FRAME_BUFFER_SIZE = 10  // Increased buffer size
-        override fun analyze(image: ImageProxy) {
-            val currentFrame = image.planes[0].buffer
-            if (lastFrames.size == FRAME_BUFFER_SIZE) {
-                if (hasMotion(lastFrames.peek(), currentFrame)) {
-                    val currentTriggerTime = System.currentTimeMillis()
-                    if (currentTriggerTime - lastTriggerTime >= MIN_TIME_BETWEEN_TRIGGERS) {
-                        onMotionDetected()
-                        lastTriggerTime = currentTriggerTime
-                    }
-                }
-                lastFrames.remove()
-            }
-            val newFrame = ByteBuffer.allocateDirect(currentFrame.capacity())
-            newFrame.put(currentFrame)
-            lastFrames.add(newFrame)
-            image.close()
-        }
-
-        private fun hasMotion(previousFrame: ByteBuffer, currentFrame: ByteBuffer): Boolean {
-            previousFrame.rewind()
-            currentFrame.rewind()
-
-            var diff = 0.0
-            while (previousFrame.hasRemaining() && currentFrame.hasRemaining()) {
-                diff += (previousFrame.get().toInt() - currentFrame.get().toInt()).toDouble().pow(2)
-            }
-
-            val averageDiff = diff / (previousFrame.limit().toDouble())
-            return averageDiff > THRESHOLD
-        }
-    }*/
-
-//sexto mejorandoel segundo
-/*
-    inner class MotionDetectionAnalyzer(private val onMotionDetected: () -> Unit) : ImageAnalysis.Analyzer {
-        private val THRESHOLD = 2500
-        private var lastFrames: Queue<ByteBuffer> = LinkedList()
-        private var lastTriggerTime = 0L
-        private val MIN_TIME_BETWEEN_TRIGGERS = 5000 // Time in milliseconds.
-        private val FRAME_BUFFER_SIZE = 5
-
-        override fun analyze(image: ImageProxy) {
-            val currentFrame = image.planes[0].buffer
-            if (lastFrames.size == FRAME_BUFFER_SIZE) {
-                if (hasMotion(lastFrames.peek(), currentFrame)) {
-                    val currentTriggerTime = System.currentTimeMillis()
-                    if (currentTriggerTime - lastTriggerTime >= MIN_TIME_BETWEEN_TRIGGERS) {
-                        onMotionDetected()
-                        lastTriggerTime = currentTriggerTime
-                    }
-                }
-                lastFrames.remove()
-            }
-            val newFrame = ByteBuffer.allocateDirect(currentFrame.capacity())
-            newFrame.put(currentFrame)
-            lastFrames.add(newFrame)
-            image.close()
-        }
-
-        private fun hasMotion(previousFrame: ByteBuffer, currentFrame: ByteBuffer): Boolean {
-            previousFrame.rewind()
-            currentFrame.rewind()
-
-            var diff = 0.0
-            while (previousFrame.hasRemaining() && currentFrame.hasRemaining()) {
-                diff += (previousFrame.get().toInt() - currentFrame.get().toInt()).toDouble().pow(2)
-            }
-
-            val averageDiff = diff / (previousFrame.limit().toDouble())
-            return averageDiff > THRESHOLD
-        }
-    }
-*/
-
-//quinto menos sensible
-/*
-inner class MotionDetectionAnalyzer(private val onMotionDetected: () -> Unit) : ImageAnalysis.Analyzer {
-    private val BASE_THRESHOLD = 60 // Base threshold.
-    private val MIN_TIME_BETWEEN_TRIGGERS = 3000 // Min time between triggers (in milliseconds).
-    private var lastFrame: ByteBuffer? = null
-    private var lastTriggerTime = 0L
-
-    override fun analyze(image: ImageProxy) {
-        val currentFrame = image.planes[0].buffer
-        lastFrame?.let { previousFrame ->
-            if (hasMotion(previousFrame, currentFrame, image)) {
-                val currentTriggerTime = System.currentTimeMillis()
-                if (currentTriggerTime - lastTriggerTime >= MIN_TIME_BETWEEN_TRIGGERS) {
-                    onMotionDetected()
-                    lastTriggerTime = currentTriggerTime
-                }
-            }
-        }
-        lastFrame = ByteBuffer.allocateDirect(currentFrame.capacity())
-        lastFrame?.put(currentFrame)
-        image.close()
-    }
-
-    private fun hasMotion(previousFrame: ByteBuffer, currentFrame: ByteBuffer, image: ImageProxy): Boolean {
-        previousFrame.rewind()
-        currentFrame.rewind()
-
-        var diff = 0
-        var totalLuminance = 0
-        while (previousFrame.hasRemaining() && currentFrame.hasRemaining()) {
-            val previousPixel = previousFrame.get().toInt()
-            val currentPixel = currentFrame.get().toInt()
-            diff += Math.abs(previousPixel - currentPixel)
-            totalLuminance += currentPixel
-        }
-
-        val averageLuminance = totalLuminance / (previousFrame.limit().toDouble())
-        val adjustedThreshold = BASE_THRESHOLD * Math.sqrt(1 - averageLuminance / 255) // Adjust threshold based on average luminance.
-
-        val averageDiff = diff.toDouble() / (previousFrame.limit().toDouble())
-        return averageDiff > adjustedThreshold
     }
 }
-*/
-
-//cuarto con variacion luminica y tiempo
-/*    inner class MotionDetectionAnalyzer(private val onMotionDetected: () -> Unit) : ImageAnalysis.Analyzer {
-        private val THRESHOLD = 40 // Base threshold.
-        private val MIN_TIME_BETWEEN_TRIGGERS = 3000 // Min time between triggers (in milliseconds).
-        private var lastFrame: ByteBuffer? = null
-        private var lastTriggerTime = 0L
-
-        override fun analyze(image: ImageProxy) {
-            val currentFrame = image.planes[0].buffer
-            lastFrame?.let { previousFrame ->
-                if (hasMotion(previousFrame, currentFrame, image)) {
-                    val currentTriggerTime = System.currentTimeMillis()
-                    if (currentTriggerTime - lastTriggerTime >= MIN_TIME_BETWEEN_TRIGGERS) {
-                        onMotionDetected()
-                        lastTriggerTime = currentTriggerTime
-                    }
-                }
-            }
-            lastFrame = ByteBuffer.allocateDirect(currentFrame.capacity())
-            lastFrame?.put(currentFrame)
-            image.close()
-        }
-
-        private fun hasMotion(previousFrame: ByteBuffer, currentFrame: ByteBuffer, image: ImageProxy): Boolean {
-            previousFrame.rewind()
-            currentFrame.rewind()
-
-            var diff = 0
-            var totalLuminance = 0
-            while (previousFrame.hasRemaining() && currentFrame.hasRemaining()) {
-                val previousPixel = previousFrame.get().toInt()
-                val currentPixel = currentFrame.get().toInt()
-                diff += Math.abs(previousPixel - currentPixel)
-                totalLuminance += currentPixel
-            }
-
-            val averageLuminance = totalLuminance / (previousFrame.limit().toDouble())
-            val adjustedThreshold = THRESHOLD * (1 - averageLuminance / 255) // Adjust threshold based on average luminance.
-
-            val averageDiff = diff.toDouble() / (previousFrame.limit().toDouble())
-            return averageDiff > adjustedThreshold
-        }
-    }*/
-
-// tercero con variacion luminica
-/*
-    inner class MotionDetectionAnalyzer(private val onMotionDetected: () -> Unit) : ImageAnalysis.Analyzer {
-        private val THRESHOLD = 30 // Este es un valor que quizás necesites ajustar.
-        private var lastFrame: ByteBuffer? = null
-
-        override fun analyze(image: ImageProxy) {
-            val currentFrame = image.planes[0].buffer
-            lastFrame?.let { previousFrame ->
-                if (hasMotion(previousFrame, currentFrame, image)) {
-                    onMotionDetected()
-                }
-            }
-            lastFrame = ByteBuffer.allocateDirect(currentFrame.capacity())
-            lastFrame?.put(currentFrame)
-            image.close()
-        }
-
-        private fun hasMotion(previousFrame: ByteBuffer, currentFrame: ByteBuffer, image: ImageProxy): Boolean {
-            previousFrame.rewind()
-            currentFrame.rewind()
-
-            var diff = 0
-            var totalLuminance = 0
-            while(previousFrame.hasRemaining() && currentFrame.hasRemaining()) {
-                val previousPixel = previousFrame.get().toInt()
-                val currentPixel = currentFrame.get().toInt()
-                diff += Math.abs(previousPixel - currentPixel)
-                totalLuminance += currentPixel
-            }
-
-            val averageLuminance = totalLuminance / (previousFrame.limit().toDouble())
-            val adjustedThreshold = THRESHOLD * (averageLuminance / 255) // Ajusta el umbral en función de la luminosidad media.
-
-            val averageDiff = diff.toDouble() / (previousFrame.limit().toDouble())
-            return averageDiff > adjustedThreshold
-        }
-    }
-*/
-
-//segundo con poca luminosidad
-/*
-inner class MotionDetectionAnalyzer(private val onMotionDetected: () -> Unit) : ImageAnalysis.Analyzer {
-         private val THRESHOLD = 50 // Aumentamos el umbral de detección.
-         private var lastFrame: ByteBuffer? = null
-         private var lastTriggerTime = 0L
-         private val MIN_TIME_BETWEEN_TRIGGERS = 5000 // Tiempo mínimo entre alarmas (en milisegundos).
-
-         override fun analyze(image: ImageProxy) {
-             val currentFrame = image.planes[0].buffer
-             lastFrame?.let { previousFrame ->
-                 if (hasMotion(previousFrame, currentFrame)) {
-                     val currentTriggerTime = System.currentTimeMillis()
-                     if (currentTriggerTime - lastTriggerTime >= MIN_TIME_BETWEEN_TRIGGERS) {
-                         onMotionDetected()
-                         lastTriggerTime = currentTriggerTime
-                     }
-                 }
-             }
-             lastFrame = ByteBuffer.allocateDirect(currentFrame.capacity())
-             lastFrame?.put(currentFrame)
-             image.close()
-         }
-
-         private fun hasMotion(previousFrame: ByteBuffer, currentFrame: ByteBuffer): Boolean {
-             previousFrame.rewind()
-             currentFrame.rewind()
-
-             var diff = 0
-             while (previousFrame.hasRemaining() && currentFrame.hasRemaining()) {
-                 diff += abs(previousFrame.get().toInt() - currentFrame.get().toInt())
-             }
-
-             val averageDiff = diff.toDouble() / (previousFrame.limit().toDouble())
-             return averageDiff > THRESHOLD
-         }
-     }
-*/
-
-//primero
-  /*  inner class MotionDetectionAnalyzer(private val onMotionDetected: () -> Unit) : ImageAnalysis.Analyzer {
-            private val THRESHOLD = 50
-            private var lastFrame: ByteBuffer? = null
-
-            override fun analyze(image: ImageProxy) {
-                val currentFrame = image.planes[0].buffer
-                lastFrame?.let { previousFrame ->
-                    if (hasMotion(previousFrame, currentFrame)) {
-                        onMotionDetected()
-                    }
-                }
-                lastFrame = ByteBuffer.allocateDirect(currentFrame.capacity())
-                lastFrame?.put(currentFrame)
-                image.close()
-            }
-
-            private fun hasMotion(previousFrame: ByteBuffer, currentFrame: ByteBuffer): Boolean {
-                previousFrame.rewind()
-                currentFrame.rewind()
-
-                var diff = 0
-                while(previousFrame.hasRemaining() && currentFrame.hasRemaining()) {
-                    diff += Math.abs(previousFrame.get().toInt() - currentFrame.get().toInt())
-                }
-
-                val averageDiff = diff.toDouble() / (previousFrame.limit().toDouble())
-                return averageDiff > THRESHOLD
-            }
-        }
-*/}
